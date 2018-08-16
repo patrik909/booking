156821a5da2010ec0a9dc2a35e3b8003c48f9a7c
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-08-16 18:06:21 +0200

    recatored code into better-sqlite3

--- server/server.js
+++ server/server.js
@@ -1,153 +1,152 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
+const betterSqlite3 = require('better-sqlite3');
+const db = connectDatabase();
+db.pragma('foreign_keys = ON');
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./booking_app.db', sqlite3.OPEN_READWRITE);
+function connectDatabase() {
+    try {
+        return new betterSqlite3('./booking_app.db', { fileMustExist: true });
+    } catch (err) {
+        const db = new betterSqlite3('./booking_app.db', {
+            fileMustExist: false,
+        });
+        const schema = fs.readFileSync('./db_schema.sql').toString();
+        db.exec(schema);
+        return db;
+    }
+}
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
-
-app.use(express.static('public'));
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+//     next();
+// });
 
 app.get('/create-details', (req, res) => {
-    createDetails(
+    const details = createDetails(
         req.query.guestId,
         req.query.numOfGuests,
         req.query.time,
-        req.query.date,
-        (err, row) => {
-            if (('express', err)) {
-                console.log(err);
-            }
-            res.send(row);
-        }
+        req.query.date
     );
+
+    res.send(details);
 });
 
 app.get('/create-guest', (req, res) => {
-    createGuest(
+    if (!req.query.firstname) {
+        const err = new Error('missing first name');
+        err.status = 400;
+        throw err;
+    }
+
+    const guest = createGuest(
         req.query.firstname,
         req.query.lastname,
         req.query.email,
-        req.query.phone,
-        (err, row) => {
-            if (err) {
-                console.log('express', err);
-            }
-            res.send(row);
-        }
+        req.query.phone
     );
+
+    res.send(guest);
 });
 
 app.get('/create-booking', (req, res) => {
-    createBooking(req.query.userId, req.query.detailsId, (err, res) => {
-        res.send(rows);
-    });
+    const booking = createBooking(req.query.guestId, req.query.detailsId);
+    res.send(booking);
 });
 
 app.get('/guests', (req, res) => {
-    getGuests((err, rows) => {
-        res.send(rows);
-    });
+    const guests = getGuests();
+    res.send(guests);
 });
 
 app.get('/booking', (req, res) => {
     getBookings((err, rows) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
         res.send(rows);
     });
 });
 
-db.get(`PRAGMA foreign_keys = ON`, () =>
-    app.listen(3000, () => console.log('Example app listening on port 3000!'))
-);
+// returns error message as json
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).send({ message: err.message });
+});
 
-const createDetails = (guestId, numOfGuests, time, date, cb) => {
-    db.serialize(() => {
-        db.run(
-            `insert
-                into details (guest_id, num_of_guests, time, date)
-            values
-                (${guestId}, ${numOfGuests}, ${time}, ${date})`,
-            function(err) {
-                if (err) {
-                    cb(err);
-                    console.log(err);
-                    return;
-                }
-                db.get(
-                    `select * from details where id = 
-                    (select seq from sqlite_sequence where name = 'details'`,
-                    cb
-                );
-            }
-        );
-    });
+const createDetails = (guestId, numOfGuests, time, date) => {
+    db.prepare(
+        `
+    INSERT
+        INTO details (guest_id, num_of_guests, time, date)
+    VALUES
+        (?, ?, ?, ?)`
+    ).run(parseInt(guestId) || null, parseInt(numOfGuests) || null, time, date);
+
+    return db
+        .prepare(
+            `select * from details where id = 
+            (select seq from sqlite_sequence where name = 'details')`
+        )
+        .get();
 };
 
-const createGuest = (firstname, lastname, email, phone, cb) => {
-    db.serialize(() => {
-        console.log('Test', firstname);
+const createGuest = (firstname, lastname, email, phone) => {
+    db.prepare(
+        `INSERT 
+            INTO guest (firstname, lastname, email, phone) 
+        VALUES
+            (?, ?, ?, ?)`
+    ).run(firstname, lastname, email, phone);
 
-        db.run(
-            `INSERT INTO guest ('firstname', 'lastname', 'email', 'phone') VALUES
-                ('${firstname}', '${lastname}', '${email}', '${phone}')`,
-            function(err) {
-                if (err) {
-                    cb(err);
-                    console.log(err.message);
-                    return;
-                }
-                db.get(
-                    `select * from guest where id = 
-                    (select seq from sqlite_sequence where name = 'guest'`,
-                    cb
-                );
-            }
-        );
-    });
+    return db
+        .prepare(
+            `SELECT 
+                * FROM guest WHERE id = 
+            (SELECT 
+                seq FROM sqlite_sequence WHERE name = 'guest')`
+        )
+        .get();
 };
 
-const createBooking = (userId, detailsId, cb) => {
-    db.serialize(() => {
-        db.run(
-            `
-        insert 
-            into booking (user_id, details_id)
-        values
-            ${userId}, ${detailsId}`,
-            function(err) {
-                if (err) {
-                    cb(err);
-                    console.log(err);
-                    return;
-                }
-                db.get(
-                    `select * from booking where id = 
-                    (select seq from sqlite_sequence where name = 'booking'`,
-                    cb
-                );
-            }
-        );
-    });
+const createBooking = (guestId, detailsId) => {
+    db.prepare(
+        `
+        INSERT
+            INTO booking (guest_id, details_id)
+        VALUES
+            (?, ?)`
+    ).run(guestId, detailsId);
+
+    return db
+        .prepare(
+            `SELECT 
+                * FROM booking WHERE id = 
+            (SELECT 
+                seq FROM sqlite_sequence WHERE name = 'booking')`
+        )
+        .get();
 };
 
-const getGuests = cb => {
-    db.all(`select * from guest`, cb);
+const getGuests = () => {
+    return db.prepare(`SELECT * FROM guest`).all();
 };
 
-const getBookings = cb => {
-    db.all(
-        `select 
-            * from booking 
-        join 
-            details on details.id = 
-            booking.details_id`,
-        cb
+const getBookings = () => {
+    return db.prepare(
+        `SELECT
+            * FROM booking 
+        JOIN
+            details ON details.id = 
+            booking.details_id`
     );
 };
+
+app.listen(4001, () => console.log('Booking app listening on port 4001!'));
