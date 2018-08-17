97ee97378d16dfdeb9837b4b291ddf1fd752a753
dorisobor <troyamber@live.com>
2018-08-17 14:52:19 +0200

    updated home with some bootstrap

--- booking-app/src/components/Home.js
+++ booking-app/src/components/Home.js
@@ -17,18 +17,37 @@ class Home extends Component {
                 <main>
 
                     <div className="content">
-                        <section>
-                            <article>
-                                <img src={TestImage} alt="test" />
-                            </article>
-                            <article>
-                                <img src={TestImage} alt="test" />
-                            </article>
-                            <article>
-                                <img src={TestImage} alt="test" />
-                            </article>
-                        </section>
+                    <div class="card bg-dark text-white">
+  <img class="card-img" src={TestImage} alt="Card image" />
+  <div class="card-img-overlay">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text">Last updated 3 mins ago</p>
+  </div>
+</div>
+
+ <div class="card bg-dark text-white">
+  <img class="card-img" src={TestImage} alt="Card image" />
+  <div class="card-img-overlay">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text">Last updated 3 mins ago</p>
+  </div>
+</div>
+
+ <div class="card bg-dark text-white">
+  <img class="card-img" src={TestImage} alt="Card image" />
+  <div class="card-img-overlay">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text">Last updated 3 mins ago</p>
+  </div>
+</div>
                     </div>
+
+
+                           
+                    
                 </main>
                 <footer>credits</footer>
             </div>
