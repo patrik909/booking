0a3ab3d8fbb39d075ac7da87877328bd5dfee3eb
patrik909 <patrik.eriksson@medieinstitutet.se>
2018-08-17 13:10:45 +0200

    added GDPR MESSAGE

--- booking-app/src/components/Booking.js
+++ booking-app/src/components/Booking.js
@@ -4,120 +4,138 @@ import DayPicker from '@kupibilet/react-day-picker';
 import '@kupibilet/react-day-picker/lib/style.css';
 
 class BookingPage extends Component {
-    state = {
-        /** --- Booking Details --- **/
-        amountOfGuests: '',
-        /** --- Guest Details --- **/
-        firstName: '',
-        lastName: '',
-        email: '',
-        phoneNumber: '',
-    };
-
-    componentDidMount() {}
+  state = {
     /** --- Booking Details --- **/
+    amountOfGuests: '',
+    /** --- Guest Details --- **/
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    submitBoxClass: 'hide',
+  };
 
-    setAmountOfGuests = event => {
-        this.setState({ amountOfGuests: event.target.value });
-    };
+  componentDidMount() {}
+  /** --- Booking Details --- **/
 
-    lol = event => {
-        console.log(event);
-        console.log('hej');
-    };
+  setAmountOfGuests = event => {
+    this.setState({ amountOfGuests: event.target.value });
+  };
 
-    /** ----- Guest Details----- **/
+  lol = event => {
+    console.log(event);
+    console.log('hej');
+  };
 
-    submitGuestDetails = event => {
-        event.preventDefault();
-        console.log(this.state.firstName);
-        console.log(this.state.lastName);
-        console.log(this.state.email);
-        console.log(this.state.phoneNumber);
+  setGuestDetails = event => {
+    event.preventDefault();
+    this.setState({ submitBoxClass: 'show' });
+  };
 
-        //        fetch('http://localhost:3000/create-guest?firstname=kalle&lastname=fdfdfd&email=fdfdfd&phone=88878')
+  /** ----- Guest Details----- **/
 
-        fetch(
-            `api/create-guest?firstname=${this.state.firstName}&lastname=${
-                this.state.lastName
-            }&email=${this.state.email}&phone=${this.state.phoneNumber}`
-        )
-            .then(response => response.json())
-            .then(fetched => {
-                console.log(fetched);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
+  submitGuestDetails = event => {
+    event.preventDefault();
+    console.log(this.state.firstName);
+    console.log(this.state.lastName);
+    console.log(this.state.email);
+    console.log(this.state.phoneNumber);
 
-    handleFirstNameInput = event => {
-        console.log(event.target.value);
-        this.setState({ firstName: event.target.value });
-    };
+    fetch(
+      `api/create-guest?firstname=${this.state.firstName}&lastname=${
+        this.state.lastName
+      }&email=${this.state.email}&phone=${this.state.phoneNumber}`
+    )
+      .then(response => response.json())
+      .then(fetched => {
+        console.log(fetched);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-    handleLastNameInput = event => {
-        this.setState({ lastName: event.target.value });
-    };
+  handleFirstNameInput = event => {
+    console.log(event.target.value);
+    this.setState({ firstName: event.target.value });
+  };
 
-    handleEmailInput = event => {
-        this.setState({ email: event.target.value });
-    };
+  handleLastNameInput = event => {
+    this.setState({ lastName: event.target.value });
+  };
 
-    handlePhoneNumberInput = event => {
-        this.setState({ phoneNumber: event.target.value });
-    };
+  handleEmailInput = event => {
+    this.setState({ email: event.target.value });
+  };
 
-    render() {
-        return (
-            <div id="BookingWrapper">
-                <div id="BookingDetails">
-                    <p>Booking Details</p>
-                    <DayPicker onClick={this.lol} />
-                    <select onChange={this.setAmountOfGuests}>
-                        <option value="1">1 Guest</option>
-                        <option value="2">2 Guests</option>
-                        <option value="3">3 Guests</option>
-                        <option value="4">4 Guests</option>
-                        <option value="5">5 Guests</option>
-                        <option value="6">6 Guests</option>
-                    </select>
-                </div>
-                <div id="GuestDetails">
-                    <p>Guest details</p>
-                    <form>
-                        <InputField
-                            type={'text'}
-                            name={'firstname'}
-                            placeholder={'First name'}
-                            handle={this.handleFirstNameInput}
-                        />
-                        <InputField
-                            type={'text'}
-                            name={'lastname'}
-                            placeholder={'Last name'}
-                            handle={this.handleLastNameInput}
-                        />
-                        <InputField
-                            type={'text'}
-                            name={'email'}
-                            placeholder={'Email'}
-                            handle={this.handleEmailInput}
-                        />
-                        <InputField
-                            type={'number'}
-                            name={'phonenumber'}
-                            placeholder={'Phone number'}
-                            handle={this.handlePhoneNumberInput}
-                        />
-                        <button type="submit" onClick={this.submitGuestDetails}>
-                            Book
-                        </button>
-                    </form>
-                </div>
-            </div>
-        );
-    }
+  handlePhoneNumberInput = event => {
+    this.setState({ phoneNumber: event.target.value });
+  };
+
+  render() {
+    return (
+      <div id="BookingWrapper">
+        <div id="BookingDetails">
+          <p>Booking Details</p>
+          <DayPicker onClick={this.lol} />
+          <select onChange={this.setAmountOfGuests}>
+            <option value="1">1 Guest</option>
+            <option value="2">2 Guests</option>
+            <option value="3">3 Guests</option>
+            <option value="4">4 Guests</option>
+            <option value="5">5 Guests</option>
+            <option value="6">6 Guests</option>
+          </select>
+        </div>
+        <div id="GuestDetails">
+          <p>Guest details</p>
+          <form>
+            <InputField
+              type={'text'}
+              name={'firstname'}
+              placeholder={'First name'}
+              handle={this.handleFirstNameInput}
+            />
+            <InputField
+              type={'text'}
+              name={'lastname'}
+              placeholder={'Last name'}
+              handle={this.handleLastNameInput}
+            />
+            <InputField
+              type={'text'}
+              name={'email'}
+              placeholder={'Email'}
+              handle={this.handleEmailInput}
+            />
+            <InputField
+              type={'number'}
+              name={'phonenumber'}
+              placeholder={'Phone number'}
+              handle={this.handlePhoneNumberInput}
+            />
+            <button type="submit" onClick={this.setGuestDetails}>
+              Next
+            </button>
+          </form>
+        </div>
+        <div id="" className={this.state.submitBoxClass}>
+          <p>
+            We're storing your personal details to enhance your experience, when
+            pressing accept you give us the right to save the information below:
+          </p>
+          <p>
+            {this.state.firstName} {this.state.lastName}
+          </p>
+          <p>{this.state.email}</p>
+          <p>{this.state.phoneNumber}</p>
+          <button type="submit" onClick={this.submitGuestDetails}>
+            Book
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BookingPage;
