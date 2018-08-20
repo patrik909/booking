55dad5734f1ffcdf317c9bd361d5aa749ec42c40
dorisobor <troyamber@live.com>
2018-08-20 07:57:25 +0200

    added footer styling

--- booking-app/src/components/Footer.js
+++ booking-app/src/components/Footer.js
@@ -6,9 +6,16 @@ class Footer extends Component {
     return (
       <div className="footerdiv">
         <footer>
-    
-        <span class="text-muted">Place sticky footer content here.</span>
-     
+        <div class="social_media_icon">
+            <a href="https://sv-se.facebook.com" target="_blank"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>
+        </div>
+        <div class="social_media_icon">
+            <a href="https://twitter.com" target="_blank"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
+        </div>
+        <div class="social_media_icon">
+            <a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+        </div>
+        <p>© Le'licious AB 2018 </p>
     </footer>
       </div>
     );
