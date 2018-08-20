51edcc289acdf00947c90eada8341927d5ffc5a5
dorisobor <troyamber@live.com>
2018-08-20 14:21:05 +0200

    added reactstrap navbar

--- booking-app/src/components/Navbar.js
+++ booking-app/src/components/Navbar.js
@@ -1,26 +1,75 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class Navbar extends Component {
+// Reactsrap navbar
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+class NavbarBootstrap extends Component {
+  constructor(props) {
+    super(props);
+
+    //  state for the hamburger navbar the state
+    //  changes depending if the dropdown is open or not
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
   render() {
     return (
       <div className="navbarDiv">
+        <Navbar color="light" light expand="md">
+          {/* <NavbarBrand href="/">reactstrap</NavbarBrand> */}
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink>
+                  <Link to="/">Home</Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                  <Link to="/Reservation">Reservation</Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                  <Link to="/Contact">Contact</Link>
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        {/*       
         <nav className="navbar navbar-expand-lg">
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/Contact">Contact</Link>
-            </li>
-            <li>
               <Link to="/Reservation">Reservation</Link>
             </li>
-          </ul>
-        </nav>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>*/}
       </div>
     );
   }
 }
 
-export default Navbar;
+export default NavbarBootstrap;
