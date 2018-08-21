cffb9ef23e3634ad87742f5694f637b1cdf2d17e
dorisobor <troyamber@live.com>
2018-08-22 00:25:44 +0200

    added jumbotron to booking page

--- booking-app/src/components/ReservationPage.js
+++ booking-app/src/components/ReservationPage.js
@@ -4,8 +4,14 @@ import Booking from './Booking';
 class Reservation extends Component {
   render() {
     return (
-      <div>
-        <h2>Reservation</h2>
+      <div className="reservation-wrapper">
+        <div className="jumbotron  jumbotron-fluid reservation-jumbo">
+          <div className="container">
+            <h1 className="display-4">Reservation</h1>
+            <p className="lead">Join in and enjoy the dining experience</p>
+          </div>
+        </div>
+
         <Booking />
       </div>
     );
