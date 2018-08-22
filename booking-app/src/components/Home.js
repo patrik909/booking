105e22475a450b9d7cf07a58df5b3d712bf21db4
dorisobor <troyamber@live.com>
2018-08-23 00:38:40 +0200

    removed some old code

--- booking-app/src/components/Home.js
+++ booking-app/src/components/Home.js
@@ -56,44 +56,6 @@ class Home extends Component {
               </div>
             </div>
           </div>
-          {/* <div className="card bg-dark text-white">
-            <img className="card-img" src={TestImage} alt="Card" />
-            <div className="card-img-overlay">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p class="card-text">Last updated 3 mins ago</p>
-            </div>
-          </div>
-
-          <div className="card bg-dark text-white">
-            <img className="card-img" src={TestImage} alt="Card" />
-            <div className="card-img-overlay">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="card-text">Last updated 3 mins ago</p>
-            </div>
-          </div>
-
-          <div className="card bg-dark text-white">
-            <img className="card-img" src={TestImage} alt="Card" />
-            <div className="card-img-overlay">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="card-text">Last updated 3 mins ago</p>
-            </div>
-          </div> */}
         </main>
       </div>
     );
