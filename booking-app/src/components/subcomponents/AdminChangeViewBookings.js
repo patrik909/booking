5c58bb05108903bfd9f976dacf95b6c2e7814ddf
patrik909 <patrik.eriksson@medieinstitutet.se>
2018-08-22 15:59:37 +0200

    local commit

--- booking-app/src/components/subcomponents/AdminChangeViewBookings.js
+++ booking-app/src/components/subcomponents/AdminChangeViewBookings.js
@@ -1,26 +1,67 @@
 import React, { Component } from 'react';
+import InputField from '../parts/InputField.js';
 import Button from '../parts/Button.js';
 
-class AdminControlButtons extends Component {
-  openCreateBooking = () => {
-    this.props.manageAdminPage('openCreateBooking');
+class AdminChangeViewBookings extends Component {
+  state = {
+    customersNameFilter: '',
   };
 
-  openViewChangeBooking = () => {
-    this.props.manageAdminPage('openViewChangeBooking');
+  handleCustomersNameFilter = event => {
+    this.setState({ customersNameFilter: event.target.value });
+    console.log(event.target.value);
   };
 
   render() {
     return (
-      <div id="adminControlButtons">
-        <Button innerText="Create Booking" onClick={this.openCreateBooking} />
-        <Button
-          innerText="View/Change Bookings"
-          onClick={this.openViewChangeBooking}
-        />
+      <div id="changeViewBooking" className={this.props.className}>
+        <header>
+          <p>Filter by name</p>
+          <InputField
+            type={'text'}
+            name={'customersNameFilter'}
+            handle={this.handleCustomersNameFilter}
+            placeholder={'Customers name'}
+          />
+        </header>
+        <div className="row">
+          <div className="col-md-3">Customers name</div>
+          <div className="col-md-2">Phone number</div>
+          <div className="col-md-3">Email</div>
+          <div className="col-md-1">Time</div>
+          <div className="col-md-2">Date</div>
+          <div />
+        </div>
+        <ul id="allBookingsList">
+          {this.props.allBookings.map((booking, i) => {
+            if (
+              booking.firstname.includes(this.state.customersNameFilter) ||
+              booking.lastname.includes(this.state.customersNameFilter)
+            ) {
+              return (
+                <li key={i} className="row">
+                  <div className="col-md-3">
+                    {booking.firstname} {booking.lastname}
+                  </div>
+                  <div className="col-md-2">{booking.phone}</div>
+                  <div className="col-md-3">{booking.email}</div>
+                  <div className="col-md-1">{booking.time}</div>
+                  <div className="col-md-2">{booking.date}</div>
+                  <div>
+                    <Button innerText={'Update'} />
+                    <Button innerText={'Delete'} />
+                  </div>
+                </li>
+              );
+            }
+          })}
+        </ul>
       </div>
     );
   }
 }
 
-export default AdminControlButtons;
+export default AdminChangeViewBookings;
+
+//            name={'filterFirstName'}
+//            handle={this.handleFirstNameInput}
