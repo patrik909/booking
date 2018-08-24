dd72b2f21b2af91f528ae13e91fc56c5b5b4a8bd
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-08-24 17:26:49 +0200

    deletion now works properly

--- booking-app/src/components/CancellationPage.js
+++ booking-app/src/components/CancellationPage.js
@@ -7,25 +7,8 @@ class CancellationPage extends Component {
     isCancellationConfirmed: false,
   };
 
-  seeBookingDetails = event => {
-    fetch(`api/booking/${event.target.value}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    })
-      .then(res => res.json())
-      .then(booking => {
-        console.log(booking);
-      })
-      .catch(error => {
-        console.log(error, 'error');
-      });
-  };
-
-  deleteBooking = event => {
-    fetch(`api/booking/${event.target.value}`, {
+  deleteBooking = () => {
+    fetch(`/api/booking/${parseInt(this.props.match.params.id, 10)}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
@@ -35,41 +18,66 @@ class CancellationPage extends Component {
       .then(booking => {
         this.setState({ isCancellationConfirmed: true });
       })
-      .catch(() => {
-        console.log('error');
+      .catch(err => {
+        console.log(err, 'error');
       });
   };
+
+  componentDidMount() {
+    fetch(`/api/booking/${parseInt(this.props.match.params.id, 10)}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(booking => {
+        this.setState({ booking });
+      })
+      .catch(error => {
+        console.log(error, 'error');
+      });
+  }
 
   render() {
     return (
       <div className="cancellation-wrapper">
         {!this.state.isCancellationConfirmed ? (
-          <React.Fragment>
-            <h2>Booking #{this.props.match.params.id}</h2>
-            <div className="btn-wrapper">
-              <button
-                value={parseInt(this.props.match.params.id, 10)}
-                onClick={this.deleteBooking}
-                type="button"
-                className="btn btn-primary btn-lg"
-              >
-                Cancel booking
-              </button>
-              <button
-                type="button"
-                className="btn btn-secondary btn-lg"
-                onClick={this.seeBookingDetails}
-                value={parseInt(this.props.match.params.id, 10)}
-              >
-                See booking details
-              </button>
+          <div className="cancellation-inner-wrapper">
+            <h2 className="cancellation-header">
+              Mange booking #{this.props.match.params.id}
+            </h2>
+            <div>
+              <div className="booking-details">
+                <h3>Booking details</h3>
+                <p>
+                  {this.state.booking.firstname +
+                    ' ' +
+                    this.state.booking.lastname}
+                </p>
+                <p>{this.state.booking.num_of_guests} people</p>
+                <p>
+                  {this.state.booking.date + ', ' + this.state.booking.time}
+                  PM
+                </p>
+              </div>
+              <div className="btn-container">
+                <button
+                  onClick={this.deleteBooking}
+                  type="button"
+                  className="btn btn-primary btn-lg"
+                >
+                  Cancel booking
+                </button>
+              </div>
             </div>
-          </React.Fragment>
+          </div>
         ) : (
           <div className="confirmation-wrapper">
             <i className="fa fa-check-circle-o fa-5x" aria-hidden="true" />
             <h3>Your booking has been cancelled</h3>
-            <p>You are always welcome to make a new reservation.</p>
+            <p>A confirmation has been sent to {this.state.booking.email}</p>
           </div>
         )}
       </div>
