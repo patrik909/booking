aae8cc26f9e0d1df2703da2969184b185a3014b4
patrik909 <patrik.eriksson@medieinstitutet.se>
2018-08-28 23:16:17 +0200

    added bootstrap divs

--- booking-app/src/components/parts/BookingDetails.js
+++ booking-app/src/components/parts/BookingDetails.js
@@ -6,18 +6,27 @@ function BookingDetails(props) {
   return (
     <div id="bookingDetails" className="row">
       <h2 className="col-12">Booking Details</h2>
-      <Datepicker getDate={props.getDate} />
-      <select className="col-12" onChange={props.setAmountOfGuests}>
-        <option value="1">1 Guest</option>
-        <option value="2">2 Guests</option>
-        <option value="3">3 Guests</option>
-        <option value="4">4 Guests</option>
-        <option value="5">5 Guests</option>
-        <option value="6">6 Guests</option>
-      </select>
-      <Button type="submit" onClick={props.submitBookingDetails}>
-        Next
-      </Button>
+      <div className="col-12">
+        <Datepicker getDate={props.getDate} />
+      </div>
+      <div className="col-12">
+        <select id="amountOfGuestsDropdown" onChange={props.setAmountOfGuests}>
+          <option value="" disabled selected>
+            Amount of Guests
+          </option>
+          <option value="1">1 Guest</option>
+          <option value="2">2 Guests</option>
+          <option value="3">3 Guests</option>
+          <option value="4">4 Guests</option>
+          <option value="5">5 Guests</option>
+          <option value="6">6 Guests</option>
+        </select>
+      </div>
+      <div className="col-12">
+        <Button type="submit" onClick={props.submitBookingDetails}>
+          Next
+        </Button>
+      </div>
     </div>
   );
 }
