fba29289d8d7e6a9d52ae7b938d967a49c0f2e4e
patrik909 <patrik.eriksson@medieinstitutet.se>
2018-08-28 22:05:04 +0200

    local commit

--- booking-app/src/components/parts/BookingDetails.js
+++ booking-app/src/components/parts/BookingDetails.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function BookingDetails(props) {
-  return <div id="bookingDetails">hej</div>;
+  return (
+    <div id="bookingDetails">
+      <h2>Booking Details</h2>
+    </div>
+  );
 }
 
 export default BookingDetails;
+
+//          <div id="BookingDetails">
+//            <Datepicker getDate={this.getDate} />
+//            <select onChange={this.setAmountOfGuests}>
+//              <option value="1">1 Guest</option>
+//              <option value="2">2 Guests</option>
+//              <option value="3">3 Guests</option>
+//              <option value="4">4 Guests</option>
+//              <option value="5">5 Guests</option>
+//              <option value="6">6 Guests</option>
+//            </select>
+//            <button type="submit" onClick={this.submitBookingDetails}>
+//              Next
+//            </button>
+//          </div>
